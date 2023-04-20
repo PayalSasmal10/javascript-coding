dcf1a8abba0f5b8c482d102172400e0c7293a72a
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-20 17:29:05 +0530

    understood closure

---
+++ coding/closure.js
@@ -0,0 +1,52 @@
+// simple closure
+function x() {
+  var a = 8;
+  function y() {
+    console.log(a);
+  }
+  y();
+}
+
+x();
+
+// advanced closure: z function stil remembering it's lexical environment and thati is called closure
+function x1() {
+  var a = 9;
+  function y() {
+    console.log(a);
+  }
+  return y;
+}
+z = x1();
+console.log(z);
+z();
+
+// 3rd example
+
+function x2() {
+  var a = 9;
+  function y() {
+    console.log(a);
+  }
+  a = 900;
+  return y;
+}
+z1 = x2();
+console.log(z1);
+z1();
+
+// 4th example
+
+function b() {
+  function x3() {
+    var a = 9;
+    function y() {
+      console.log(a);
+    }
+    a = 900;
+    return y;
+  }
+  z1 = x2();
+  console.log(z1);
+  z1();
+}
