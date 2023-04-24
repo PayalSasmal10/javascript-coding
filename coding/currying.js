17f150186c13958b669aadde4ddafa06932e9a4f
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-24 20:26:03 +0530

    learning currying

--- coding/currying.js
+++ coding/currying.js
@@ -1,9 +1,32 @@
 // advanced way to invoke the function. we can currying our function in 2 ways
 
 // 1.using bind
-function multiply(x,y){
-    console.log(x * y);
+function multiply(x, y) {
+  console.log(x * y);
 }
 
 let multiplyByTwo = multiply.bind(this, 2);
+// let multiplyByTwo = multiply.bind(this,5, 2); if we are using this then the down multiplyByTwo will be ignored.
 multiplyByTwo(3);
+
+// 2. using closure
+
+let multiplyCloser = function (x) {
+  return function (y) {
+    console.log(x * y);
+  };
+};
+
+let multiplying = multiplyCloser(7, 8);
+multiplying(6);
+
+// infinite currying
+
+function add(x) {
+  return function (y) {
+    if (y) return add(x + y);
+    return x;
+  };
+}
+
+console.log(add(4)(5)(1)());
