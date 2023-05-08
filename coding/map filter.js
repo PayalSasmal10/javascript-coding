90f68aa2d00d2d3d063009f364722978867aa092
Payal Sasmal <sasmalpayal@gmail.com>
2023-05-08 23:32:31 +0530

    map, filter and reduce function creation

--- coding/map filter.js
+++ coding/map filter.js
@@ -1,25 +1,46 @@
 let arr = [1, 2, 3, 4, 5];
 
 // using map function
-/*let square = arr.map(currentItems => currentItems ** 2);
-console.log(square);*/
+let squares = arr.map((currentItems) => currentItems ** 2);
+console.log(squares);
 
 // creating own map
+console.log("created own map function........");
+function customMap(arr, callbackFunc) {
+  let mapArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const result = callbackFunc(arr[i]);
+    mapArr.push(result);
+  }
 
-function customMap(arr, callbackFunc){
-    let mapArr = [];
-    for(let i=0; i<arr.length; i++){
-        const result = callbackFunc(arr[i]);
-        mapArr.push(result);
-    }
-
-    return mapArr;
-
+  return mapArr;
 }
 
-let square = customMap(arr, function(currentItem) { 
-    return currentItem ** 2
+let square = customMap(arr, function (currentItem) {
+  return currentItem ** 2;
 });
 
 console.log(square);
 
+console.log("using in-built filter function........");
+// filter
+let fillteredValue = arr.filter((x) => x % 2 == 0);
+console.log(fillteredValue);
+
+// custom filter function
+console.log("created own filter function........");
+function customFilter(arr, callBackfn) {
+  let filterArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const result = callBackfn(arr[i]);
+    if (result) {
+      filterArr.push(arr[i]);
+    }
+  }
+  return filterArr;
+}
+
+let custFilter = customFilter(arr, (currentVariable) => {return currentVariable%2 == 0;}
+);
+
+console.log(custFilter);
