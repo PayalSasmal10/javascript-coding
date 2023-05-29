70fb0691adc353846796f987355cf5fe4b9e1065
Payal Sasmal <sasmalpayal@gmail.com>
2023-05-29 22:23:04 +0530

    playing with objects

---
+++ coding/objects/iteratorsObjects.js
@@ -0,0 +1,13 @@
+// using for... of
+// iterate over an array
+
+for (const x of [1, 2, 3, 5]){
+    console.log(x);
+}
+
+console.log("using for...of...................");
+// iterate over a string
+
+for(const y of "test"){
+    console.log(y);
+}
