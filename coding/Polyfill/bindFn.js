a6fced8e2aafb5ad3ab7ed5ea4e7ebaebe9e22ee
Payal Sasmal <sasmalpayal@gmail.com>
2023-06-26 20:28:32 +0530

    bind polyfill in js

---
+++ coding/Polyfill/bindFn.js
@@ -0,0 +1,27 @@
+let objModified = {
+    name: "Oli",
+}
+
+let obj = {
+    name: "Payal",
+    getName : function (state) {
+        console.log(this.name + " is from " + state);
+    }
+
+}
+
+let returnBind = obj.getName.bind(objModified, "WB");
+returnBind();
+
+// Polyfill
+
+Function.prototype.myBind = function (obj, ...args) {
+    obj.myMethod = this;
+    console.log(obj);
+    return function () {
+        obj.myMethod(...args);
+    }
+}
+
+let returnMyCustPolyFill = obj.getName.myBind(obj, "Karnataka");
+returnMyCustPolyFill();
