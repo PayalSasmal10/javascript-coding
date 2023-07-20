50002a9c788ea07301b721fce68baccbf7adf58b
Payal Sasmal <sasmalpayal@gmail.com>
2023-07-20 14:39:45 +0530

    objects methods

---
+++ coding/objects/objectsKeys.js
@@ -0,0 +1,5 @@
+const objs = {'1' : 1, '3': 3, '2': 2, '7': 7, '6': 6, '5': 5, '4' : 4};
+console.log(objs);
+console.log(Object.keys(objs));
+console.log(Object.values(objs));
+console.log(Object.values(objs).length);
