392e0ca55395157e05cfcabde36b997d0bd2b1f1
Payal Sasmal <sasmalpayal@gmail.com>
2023-07-24 13:17:27 +0530

    map in es6

---
+++ coding/DSA/map.js
@@ -0,0 +1,18 @@
+// MAP is a new DS which is introduced in ES6. It is returned a key value data.
+
+let m = new Map([
+    [1,2],
+    [2,3],
+    [3,4],
+    [4,5]
+]);
+
+console.log(m);
+m.set("newData", "newValue");
+console.log(m);
+console.log(m.has(1));
+console.log(m.has("test"));
+console.log(m.delete(1));
+console.log(m);
+m.clear();
+console.log(m);
