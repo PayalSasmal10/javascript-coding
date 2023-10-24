744d3eb182b5b01b67f3bf1792557a0173b130d6
Payalsasmal10 <sasmalpayal@gmail.com>
2023-10-24 15:10:44 +0530

    learning basic js

---
+++ coding/javascript understanding/wayOfHandlingVariables.js
@@ -0,0 +1,24 @@
+function test(){
+    var a=b=3;
+    console.log(a);
+    console.log(b);
+  }
+  
+  test();
+  console.log("outside of teh func",b);
+//   console.log(a);
+
+function interchangbleObj(){
+
+    var x = {},
+        y = {'key': 'y'},
+        z = {'key': 'z'};
+  
+  console.log(x);
+  console.log(y);
+  console.log(z);
+  x[y] = 123;
+  x[z] = 456;
+  console.log("last x", x);
+}
+interchangbleObj();
