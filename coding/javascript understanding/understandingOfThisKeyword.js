af28e8f742683571ee7dd322e12081685c946740
Payalsasmal10 <sasmalpayal@gmail.com>
2023-10-24 16:58:08 +0530

    learning basic js

--- coding/javascript understanding/understandingOfThisKeyword.js
+++ coding/javascript understanding/understandingOfThisKeyword.js
@@ -1,1 +1,35 @@
-let 
+let hero = {
+    _name: "John Deo",
+    getSecurityIdentity: function(){
+        return this._name;
+    },
+    getArroFuncSecurityIdentity: () => {
+        this._name;
+    }
+}
+
+let storeSecurityIdentity = hero.getSecurityIdentity.bind(hero);
+console.log(storeSecurityIdentity());
+console.log(hero.getSecurityIdentity());
+
+console.log("......................................................");
+// 2nd way
+
+var lenggth = 10;
+function fn() {
+	console.log(this.lenggth);
+}
+
+fn();
+// var obj = {
+//   length: 5,
+//   method: function(fn) {
+//     fn();
+//     console.log(arguments[0]);
+//     arguments[0]();
+//   }
+// };
+
+// obj.method(fn, 1);
+
+console.log("..................................");
