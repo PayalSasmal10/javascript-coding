6b34686d92a7c149e9fb94f836176db8879d8438
Payalsasmal10 <sasmalpayal@gmail.com>
2023-10-25 11:58:22 +0530

    working with promises

--- coding/promise/promiseAll.js
+++ coding/promise/promiseAll.js
@@ -2,11 +2,27 @@ let promise1 = Promise.resolve(3);
 console.log(promise1);
 let promise2 = 42;
 let promise3 = new Promise((resolve, reject) => {
-    setTimeout(reject, 100, 'test');
+    setTimeout(resolve, 100, 'test');
 });
 
 Promise.all([promise1, promise2, promise3]).then((value) => {
     console.log(value);
 });
 
+// Akshay saini examples
+console.log("Akshay saini examples......");
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p1 success"), 3000);
+});
 
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p2 success"), 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p3 success"), 2000);
+});
+
+Promise.all([p1, p2, p3])
+.then((res) => console.log(res))
+.catch((err) => console.error(err));
