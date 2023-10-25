34f7c5dd03b39c3fffdcb53cc49a9522501da9dc
Payalsasmal10 <sasmalpayal@gmail.com>
2023-10-25 12:24:22 +0530

    working with promises

--- coding/promise/promiseRace.js
+++ coding/promise/promiseRace.js
@@ -6,9 +6,17 @@ const p1 = new Promise((resolve, reject) => {
 });
 
 const p2 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("p2 success"), 1000);
+    setTimeout(() => reject("p2 Fail"), 5000);
 });
 
 const p3 = new Promise((resolve, reject) => {
     setTimeout(() => resolve("p3 success"), 2000);
+});
+
+// whatever first promise got setteled that value will be 
+Promise.race([p1, p2, p3])
+.then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
 });
